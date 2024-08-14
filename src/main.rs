41c52971b103c0bbb734c79f9fa4974ce79ec8bc
ASIA-PACIFIC\xiaocz <xiaochun.zhou@dell.com>
2024-08-14 10:33:00 +0100

    listen on all ipv4 addr

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use get_if_addrs::{get_if_addrs, IfAddr};
 use tokio::net::TcpListener;
 
 #[tokio::main]
@@ -6,11 +7,11 @@ async fn main() {
     let routes = warp::fs::dir("./assets");
 
     // Try to bind to port 8000
-    let port = match TcpListener::bind("127.0.0.1:8000").await {
+    let port = match TcpListener::bind("0.0.0.0:8000").await {
         Ok(listener) => listener.local_addr().unwrap().port(),
         Err(_) => {
             // If port 8000 is unavailable, bind to a random port
-            TcpListener::bind("127.0.0.1:0")
+            TcpListener::bind("0.0.0.0:0")
                 .await
                 .unwrap()
                 .local_addr()
@@ -19,17 +20,35 @@ async fn main() {
         }
     };
 
+    let ip = get_if_addrs()
+        .unwrap()
+        .into_iter()
+        .find(|iface| {
+            !iface.is_loopback()
+                && match iface.addr {
+                    IfAddr::V4(ref _addr) => true,
+                    IfAddr::V6(_) => false,
+                }
+        })
+        .map(|iface| match iface.addr {
+            IfAddr::V4(ref addr) => addr.ip.to_string(),
+            IfAddr::V6(_) => unreachable!(), // We've already filtered for V4 above
+        })
+        .unwrap_or_else(|| "127.0.0.1".to_string());
+
     println!(
-        "Serving files from the current directory on http://localhost:{}",
-        port
+        "Serving files from the current directory on http://{}:{}",
+        ip, port
     );
 
     #[cfg(any(target_os = "windows"))] //, target_os = "macos"
     {
         // Open browser
-        open::that(format!("http://localhost:{}", port)).unwrap();
+        open::that(format!("http://{}:{}", ip, port)).unwrap();
     }
 
+    let server_addr = ([0, 0, 0, 0], port);
+
     // Start the server
-    warp::serve(routes).run(([127, 0, 0, 1], port)).await;
+    warp::serve(routes).run(server_addr).await;
 }
