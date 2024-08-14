94c5e39af32e600802af3e235474267a65cb1c5c
ASIA-PACIFIC\xiaocz <xiaochun.zhou@dell.com>
2024-08-14 07:55:39 +0100

    intial version

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # zhttpd
-zero config http server
+a light http server without any configuration, suitable for doing testing locally.
+
+launch zhttpd, put html files to 'assets' folder.
+it will start browser automatically after you launched zhttpd.
---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    // Serve files from the assets directory
+    let routes = warp::fs::dir("./assets");
+
+    // Try to bind to port 8000
+    let port = match TcpListener::bind("127.0.0.1:8000").await {
+        Ok(listener) => listener.local_addr().unwrap().port(),
+        Err(_) => {
+            // If port 8000 is unavailable, bind to a random port
+            TcpListener::bind("127.0.0.1:0").await.unwrap().local_addr().unwrap().port()
+        }
+    };
+
+    println!("Serving files from the current directory on http://localhost:{}", port);
+
+    // Open browser
+    open::that(format!("http://localhost:{}", port)).unwrap();
+
+    // Start the server
+    warp::serve(routes).run(([127, 0, 0, 1], port)).await;
+}
